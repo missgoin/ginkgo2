82ea84781db8530c24047d3b794a0c0d30599536
missgoin <mister.goindo@gmail.com>
2023-06-16 21:44:06 +0700

    fix selinux

--- .circleci/ginkgoaosp.sh
+++ .circleci/ginkgoaosp.sh
@@ -5,7 +5,7 @@ ZIPNAME="Super.Kernel_$(TZ=Asia/Jakarta date +"%Y%m%d-%H%M").zip"
 TC_DIR="$PWD/tc/r487747"
 GCC_64_DIR="$PWD/tc/aarch64-linux-android-4.9"
 GCC_32_DIR="$PWD/tc/arm-linux-androideabi-4.9"
-AK3_DIR="$PWD/android/AnyKernel3"
+#AK3_DIR="$PWD/AnyKernel3"
 DEFCONFIG="ginkgo-perf_defconfig"
 
 export PATH="$TC_DIR/bin:$PATH"
@@ -37,11 +37,11 @@ exit 1
 fi
 fi
 
-if [[ $1 = "-r" || $1 = "--regen" ]]; then
-make O=out ARCH=arm64 $DEFCONFIG savedefconfig
-cp out/defconfig arch/arm64/configs/$DEFCONFIG
-exit
-fi
+#if [[ $1 = "-r" || $1 = "--regen" ]]; then
+#make O=out ARCH=arm64 $DEFCONFIG savedefconfig
+#cp out/defconfig arch/arm64/configs/$DEFCONFIG
+#exit
+#fi
 
 if [[ $1 = "-c" || $1 = "--clean" ]]; then
 rm -rf out
@@ -51,22 +51,36 @@ mkdir -p out
 make O=out ARCH=arm64 $DEFCONFIG
 
 echo -e "\nStarting compilation...\n"
-make -j$(nproc --all) O=out ARCH=arm64 CC=clang LD=ld.lld AR=llvm-ar AS=llvm-as NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip CROSS_COMPILE=$GCC_64_DIR/bin/aarch64-linux-android- CROSS_COMPILE_ARM32=$GCC_32_DIR/bin/arm-linux-androideabi- CLANG_TRIPLE=aarch64-linux-gnu- Image.gz-dtb dtbo.img
+make -j$(nproc --all) O=out ARCH=arm64 \
+    CC=clang \
+    LD=ld.lld \
+    AR=llvm-ar \
+    AS=llvm-as \
+    NM=llvm-nm \
+    OBJCOPY=llvm-objcopy \
+    OBJDUMP=llvm-objdump \
+    STRIP=llvm-strip \
+    CROSS_COMPILE=$GCC_64_DIR/bin/aarch64-linux-android- \
+    CROSS_COMPILE_ARM32=$GCC_32_DIR/bin/arm-linux-androideabi- \
+    CLANG_TRIPLE=aarch64-linux-gnu- \
+    Image.gz-dtb dtbo.img
 
 if [ -f "out/arch/arm64/boot/Image.gz-dtb" ] && [ -f "out/arch/arm64/boot/dtbo.img" ]; then
 echo -e "\nKernel compiled succesfully! Zipping up...\n"
-if [ -d "$AK3_DIR" ]; then
-cp -r $AK3_DIR AnyKernel3
-elif ! git clone -q https://github.com/missgoin/AnyKernel3; then
-echo -e "\nAnyKernel3 repo not found locally and cloning failed! Aborting..."
-exit 1
-fi
+
+#if [ -d "$AK3_DIR" ]; then
+#cp -r $AK3_DIR AnyKernel3
+#elif ! git clone --depth=1 https://github.com/missgoin/AnyKernel3; then
+#echo -e "\nAnyKernel3 repo not found locally and cloning failed! Aborting..."
+#exit 1
+#fi
+git clone --depth=1 https://github.com/missgoin/AnyKernel3.git
+
 cp out/arch/arm64/boot/Image.gz-dtb AnyKernel3
 cp out/arch/arm64/boot/dtbo.img AnyKernel3
 rm -f *zip
 cd AnyKernel3
-git checkout master &> /dev/null
-zip -r9 "../$ZIPNAME" * -x '*.git*' README.md *placeholder
+zip -r9 "../$ZIPNAME" *
 cd ..
 rm -rf AnyKernel3
 rm -rf out/arch/arm64/boot
